6951784aa7e22d53ff9aaabeb8172f39ebd278db
robinrd <rich0650@algonquinlive.com>
2016-03-22 12:15:50 -0400

    Created index.php

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>ReachCanada</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-6">
+                <span class="reachlogo"><img src="images/reach-logo.png" alt="Reach Logo"></span>
+            </div>
+            <div class="col-xs-4">
+            </div>
+            <div class="col-xs-2">
+                <p>Francais</p>
+            </div>
+        </div>
+    </div>
+   <header>
+       <nav>
+           <ul>
+               <li>Home</li>
+               <li>Who We Are</li>
+               <li>What We Do</li>
+               <li>Events</li>
+               <li>Resources</li>
+               <li>Contact Us</li>
+           </ul>
+       </nav>
+       <div class="donate-btn">
+           <p>DONATE NOW</p>
+       </div>
+   </header>
+    <div class="container">
+    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+      </ol>    
+      
+      <div class="carousel-inner" role="listbox">
+
+        <div class="item active">
+            <a href="#"><img src="images/placeholder-img.png" alt="..."></a>
+              <div class="carousel-caption">
+                <h3>A title</h3>
+                <p>A brief description</p>
+              </div>
+        </div>
+ 
+        <div class="item">
+            <a href="#"><img src="images/placeholder-img-2.png" alt="..."></a>
+              <div class="carousel-caption">
+                <h3>A title</h3>
+                <p>A brief description</p>
+              </div>
+        </div>
+ 
+        <div class="item">
+            <a href="#"><img src="images/placeholder-img-3.png" alt="..."></a>
+              <div class="carousel-caption">
+                <h3>A title</h3>
+                <p>A brief description</p>
+              </div>
+        </div>
+ 
+      </div>
+ 
+      <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+</div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <script src="js/js.php"></script>
+</body>
+</html>
+   
